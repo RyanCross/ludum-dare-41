f87e1e3e3146c8e5b6c6c193093302ceb077323e
warrenscipio <warrenscipio@gmail.com>
2018-04-21 18:18:35 -0500

    Map-layout level 1, has slow grass

---
+++ Assets/Scripts/slowPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slowPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.gameObject.tag == "Player")
+		{
+			print ("Enter Trigger");
+			col.gameObject.SendMessage("slowed");
+			// Call the slow down function or set the new movement speed
+		}
+	}
+	void OnTriggerExit(Collider col)
+	{
+		if(col.gameObject.tag == "Player")
+		{
+			print ("Exit Trigger");
+			col.gameObject.SendMessage("normalSpeed");
+			// Call the slow down function or set the new movement speed
+		}
+	}
+}
