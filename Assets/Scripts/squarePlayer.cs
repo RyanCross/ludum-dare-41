646816cbaa4657f7f8b64c8ea3932e373b6536e3
warrenscipio <warrenscipio@gmail.com>
2018-04-21 10:33:19 -0500

    Basic map layout with moving square

---
+++ Assets/Scripts/squarePlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class squarePlayer : MonoBehaviour {
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		moveSpeed = 10;
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Translate (moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime, 0f,moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime);
+	}
+}
