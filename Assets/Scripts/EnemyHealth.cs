4dedcf9973c2074e2ac5df2aac7a45ecb25177eb
mattcharles <matthew.c.anthony.miller@gmail.com>
2018-04-20 23:28:43 -0500

    Enemy scripts

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyHealth : MonoBehaviour {
+    public int startingHealth = 100;
+    public int currentHealth;
+    public float sinkSpeed = 2.5f;
+    public int baseScore = 10;
+    public AudioClip deathClip;
+
+    Animator anim;
+    AudioSource enemyAudio;
+    CapsuleCollider capsuleCollider;
+    bool isDead;
+    bool isSinking;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        enemyAudio = GetComponent<AudioSource>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+
+        currentHealth = startingHealth;
+    }
+
+    private void Update()
+    {
+        if (isSinking)
+        {
+            transform.Translate(-Vector3.up * sinkSpeed * Time.deltaTime);
+        }
+    }
+
+    public void TakeDamage(int amount, Vector3 hitPoint, bool fromPlayer)
+    {
+        if (isDead)
+            return;
+
+        enemyAudio.Play();
+
+        currentHealth -= amount;
+
+        if (currentHealth <= 0)
+        {
+            Death(fromPlayer);
+        }
+    }
+
+    public void Death(bool fromPlayer)
+    {
+        isDead = true;
+
+        capsuleCollider.isTrigger = true;
+
+        anim.SetTrigger("Dead");
+
+        enemyAudio.clip = deathClip;
+        enemyAudio.Play();
+        //ScoreManager.score += scoreValue;
+    }
+
+    public void StartSinking()
+    {
+        GetComponent<NavMeshAgent>().enabled = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+        isSinking = true;
+        Destroy(gameObject, 2f);
+    }
+}
