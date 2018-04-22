63e63bfa36ff883c8564287b7f8b5f923482a561
mattcharles <matthew.c.anthony.miller@gmail.com>
2018-04-21 20:39:08 -0500

    Possible shop layout. Added all spawn points to MapLayout

---
+++ Assets/Scripts/Shop/BuildManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+    public static BuildManager instance;
+
+	void Awake () {
+        if (instance != null)
+        {
+            Debug.LogError("Multiple BuildManagers in scene!");
+            return;
+        }
+        instance = this;
+	}
+
+    public GameObject standardTowerPrefab;
+
+    private GameObject towerToBuild;
+    public GameObject otherTowerPrefab;
+
+    public GameObject GetTowerToBuild()
+    {
+        return towerToBuild;
+    }
+
+    public void SetTowerToBuild(GameObject tower)
+    {
+        towerToBuild = tower;
+    }
+}
